742b8388b08aa90fa9df04436d65fc93b8634d1c
Yann Fischer <yann.fischer.pro@gmail.com>
2025-04-24 11:58:24 +0200

    spectrum video fixed

--- SHAnalysis/spectrum_ploting.m
+++ SHAnalysis/spectrum_ploting.m
@@ -1,8 +1,10 @@
-function spectrum_ploting(SH, f_signal, f_signal_bkg, mask_artery, mask_bkg, fs, f1, f2)
+function spectrum_video = spectrum_ploting(SH, f_signal, f_signal_bkg, mask_artery, mask_bkg, fs, f1, f2)
 %SPECTRUM_PLOTING Summary of this function goes here
 %   Detailed explanation goes here
 
-[~, ~, numFreq] = size(SH);
+[~, ~, numFreq, numFrames] = size(SH);
+
+spectrum_video = zeros(420, 560, 3, numFrames);
 
 f_signal_idx = f_signal * numFreq / fs;
 f_signal_idx_bkg = f_signal_bkg * numFreq / fs;
@@ -10,61 +12,70 @@ f_signal_idx_bkg = f_signal_bkg * numFreq / fs;
 SH_artery = squeeze(sum(SH .* mask_artery, [1 2])) / nnz(mask_artery);
 SH_bkg = squeeze(sum(SH .* mask_bkg, [1 2])) / nnz(mask_bkg);
 
-I_f = fftshift(log10(SH_artery(round(f_signal_idx))));
-I_f_bkg = fftshift(log10(SH_bkg(round(f_signal_idx_bkg))));
-
 axis_x = linspace(-fs / 2, fs / 2, numFreq);
-
-p_artery = plot(axis_x, fftshift(log10(SH_artery)), 'red', 'LineWidth', 1.5, 'DisplayName', 'Arteries');
 sclingrange = abs(fftshift(axis_x)) > f1;
-yrange = [.99 * log10(min(SH_artery(sclingrange))) 1.01 * log10(max(SH_artery(sclingrange)))];
-ylim(yrange);
-xlim([-fs / 2 fs / 2]);
-rectangle('Position', [-f1 yrange(1) 2 * f1 (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-rectangle('Position', [-fs / 2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-rectangle('Position', [f2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-om_RMS_line = line([-f_signal f_signal], [I_f I_f]);
-om_RMS_line.Color = 'red';
-om_RMS_line.LineStyle = '-';
-om_RMS_line.Marker = '|';
-om_RMS_line.MarkerSize = 12;
-om_RMS_line.LineWidth = 1;
-om_RMS_line.Tag = 'f RMS';
-text(0, I_f, 'f_{RMS}', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom')
+ymin = log10(min(SH_artery(sclingrange, :), [], 'all'));
+ymax = log10(max(SH_artery(sclingrange, :), [], 'all'));
 
-xline(f1, '--')
-xline(f2, '--')
-xline(-f1, '--')
-xline(-f2, '--')
-xticks([-f2 -f1 0 f1 f2])
-xticklabels({num2str(round(-f2, 1)), num2str(round(-f1, 1)), '0', num2str(round(f1, 1)), num2str(round(f2, 1))})
-% annotation('textarrow',omegaAVG_left,omegaAVG_right,'String','omega','FontSize',13,'Linewidth',2)
-title('Average spectrum')
+parfor frameIdx = 1:numFrames
 
-% plot(fullTime,fullArterialPulse,'-k', fullTime,fullBackgroundSignal,':k', fullTime, fullVenousSignal, '-.k', 'LineWidth',2) ;
-% title('arterial pulse waveform and background signal'); % averaged outside of segmented vessels
-fontsize(gca, 12, "points");
-xlabel('frequency (kHz)', 'FontSize', 14);
-ylabel('log10 S', 'FontSize', 14);
-pbaspect([1.618 1 1]);
-set(gca, 'LineWidth', 1);
-uistack(p_artery, 'top');
-uistack(gca, 'top');
+    fi = figure("Visible", "on", "Color", 'w');
 
-hold on
-plot(axis_x, fftshift(log10(SH_bkg)), 'black--', 'LineWidth', 1.5, 'DisplayName', 'Background');
-om_RMS_line = line([-f_signal_bkg f_signal_bkg], [I_f_bkg I_f_bkg]);
-om_RMS_line.Color = 'black';
-om_RMS_line.LineStyle = '-';
-om_RMS_line.Marker = '|';
-om_RMS_line.MarkerSize = 12;
-om_RMS_line.LineWidth = 1;
-om_RMS_line.Tag = 'f RMS';
-text(0, I_f_bkg, 'f_{RMS background}', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'top')
-legend('', '', '', '', '', 'Arteries', 'Background');
+    SH_t = SH_artery(:, frameIdx);
+    SH_bkg_t = SH_bkg(:, frameIdx);
 
-pbaspect([1.618 1 1])
-box on
-set(gca, 'LineWidth', 2)
+    I_f = fftshift(log10(SH_t(round(f_signal_idx(frameIdx)))));
+    I_f_bkg = fftshift(log10(SH_bkg_t(round(f_signal_idx_bkg(frameIdx)))));
+
+    rectangle('Position', [-f1 ymin 2*f1 (ymax-ymin)], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
+    hold on
+
+    plot(axis_x, fftshift(log10(SH_t)), 'red', 'LineWidth', 1.5, 'DisplayName', 'Arteries');
+    ylim([ymin ymax]);
+    xlim([-fs / 2 fs / 2]);
+    om_RMS_line = line([-f_signal(frameIdx) f_signal(frameIdx)], [I_f I_f]);
+    om_RMS_line.Color = 'red';
+    om_RMS_line.LineStyle = '-';
+    om_RMS_line.Marker = '|';
+    om_RMS_line.MarkerSize = 12;
+    om_RMS_line.LineWidth = 1;
+    om_RMS_line.Tag = 'f RMS';
+    text(0, I_f, 'f_{RMS}', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'bottom')
+
+    plot(axis_x, fftshift(log10(SH_bkg_t)), 'black--', 'LineWidth', 1.5, 'DisplayName', 'Background');
+    om_RMS_line = line([-f_signal_bkg(frameIdx) f_signal_bkg(frameIdx)], [I_f_bkg I_f_bkg]);
+    om_RMS_line.Color = 'black';
+    om_RMS_line.LineStyle = '-';
+    om_RMS_line.Marker = '|';
+    om_RMS_line.MarkerSize = 12;
+    om_RMS_line.LineWidth = 1;
+    om_RMS_line.Tag = 'f RMS';
+    text(0, I_f_bkg, 'f_{RMS background}', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'top')
+
+    xline(f1, '--')
+    xline(f2, '--')
+    xline(-f1, '--')
+    xline(-f2, '--')
+    xticks([-f2 -f1 0 f1 f2])
+    xticklabels({num2str(round(-f2, 1)), num2str(round(-f1, 1)), '0', num2str(round(f1, 1)), num2str(round(f2, 1))})
+    % annotation('textarrow',omegaAVG_left,omegaAVG_right,'String','omega','FontSize',13,'Linewidth',2)
+    title('Average spectrum')
+
+    % plot(fullTime,fullArterialPulse,'-k', fullTime,fullBackgroundSignal,':k', fullTime, fullVenousSignal, '-.k', 'LineWidth',2) ;
+    % title('arterial pulse waveform and background signal'); % averaged outside of segmented vessels
+
+    fontsize(gca, 14, "points");
+    xlabel('frequency (kHz)', 'FontSize', 14);
+    ylabel('log10 S', 'FontSize', 14);
+    pbaspect([1.618 1 1]);
+    box on
+    set(gca, 'LineWidth', 2);
+
+    legend('Arteries', '', 'Background');
+
+    frame = getframe(fi);
+    spectrum_video(:, :, :, frameIdx) = frame.cdata;
+    close(fi)
+end
 
 end
--- SHAnalysis/spectrum_video.m
+++ SHAnalysis/spectrum_video.m
@@ -1,7 +1,7 @@
 function spectrum_video(SH, f_RMS_video, maskArtery, maskBkg)
 
 ToolBox = getGlobalToolBox;
-[numX, numY, ~, numFrames] = size(SH);
+[numX, numY, ~, ~] = size(SH);
 f1 = ToolBox.f1;
 f2 = ToolBox.f2;
 fs = ToolBox.fs;
@@ -9,21 +9,13 @@ fs = ToolBox.fs;
 f_signal = squeeze(sum(f_RMS_video .* maskArtery, [1 2]) / nnz(maskArtery));
 f_signal_bkg = squeeze(sum(f_RMS_video .* maskBkg, [1 2]) / nnz(maskBkg));
 
-maskArteryResized = logical(imresize(maskArtery, [numX, numY]));
-maskBkgResized = logical(imresize(maskBkg, [numX, numY])) & ~maskArteryResized;
+maskArtery = logical(imresize(maskArtery, [numX, numY]));
+maskBkg = logical(imresize(maskBkg, [numX, numY])) & ~maskArtery;
 
-spectrum_video = zeros(420, 560, 3, numFrames);
 % make video
-parfor frameIdx = 1:numFrames
-    fi = figure("Visible", "off", "Color", 'w');
-    spectrum_ploting(SH(:, :, :, frameIdx), f_signal(frameIdx), f_signal_bkg(frameIdx), ...
-        maskArteryResized, maskBkgResized, fs, f1, f2);
 
-    frame = getframe(fi);
-    spectrum_video(:, :, :, frameIdx) = frame.cdata;
-    close(fi)
-end
+spectrum_video = spectrum_ploting(SH, f_signal, f_signal_bkg, maskArtery, maskBkg, fs, f1, f2);
 
 writeVideoOnDisc(spectrum_video, fullfile(ToolBox.path_avi, strcat(ToolBox.main_foldername, '_spectrum_video')));
-writeGifOnDisc(mat2gray(spectrum_video), "spectrum_video.gif")
+writeGifOnDisc(mat2gray(spectrum_video), "spectrum_video")
 end
