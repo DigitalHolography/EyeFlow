da8d6b372b536ee4b34f0765d9cb6bb3f8535643
Yann Fischer <yann.fischer.pro@gmail.com>
2025-05-20 17:16:09 +0200

    HoloNet from huggingface

--- Segmentation/getHolonetprediction.m
+++ Segmentation/getHolonetprediction.m
@@ -1,8 +1,14 @@
 function mask = getHolonetprediction(M0, net)
 
 if nargin < 2 || isempty(net)
-    net = importONNXNetwork("Models\unet_resnet34.onnx");
+    url = 'https://huggingface.co/noTban/uresnet/resolve/main/uresnet.onnx';
+    websave('Models\uresnet.onnx', url);
+    net = importONNXNetwork('Models\uresnet.onnx');
 end
+
+% if nargin<2 || isempty(net)
+%     net = importONNXNetwork("Models\unet_resnet34.onnx");
+% end
 
 [Nx, Ny] = size(M0);
 
@@ -12,7 +18,7 @@ input = rescale(M0);
 
 output = predict(net, input);
 
-mask = sigmoid(output(:, :, 2)) > 0.5;
+mask = sigmoid(output) > 0.5;
 
 mask = imresize(mask, [Nx, Ny]);
 end
