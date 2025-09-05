a382f47a801d2bfdddda336d63e20bf350d794ef
Marius Dubosc <mariusdubosc@gmail.com>
2025-09-05 14:19:48 +0200

    chore: fixed transformation vector; still needs fixing

--- NonRigidMasks/nonRigidMask.m
+++ NonRigidMasks/nonRigidMask.m
@@ -1,4 +1,4 @@
-function warp_mask_with_demons(frame1Path, frame2Path, mask1Path, outMaskPath)
+ function warp_mask_with_demons(frame1Path, frame2Path, mask1Path, outMaskPath)
 
     I1 = imread(frame1Path);
     I2 = imread(frame2Path);
@@ -15,16 +15,16 @@ function warp_mask_with_demons(frame1Path, frame2Path, mask1Path, outMaskPath)
     I1 = im2double(I1);
     I2 = im2double(I2);
 
-    [optimizer, metric] = imregconfig('monomodal');
+    [optimizer, metric] = imregconfig("monomodal");
     optimizer.MaximumIterations = 100;
-    tformRigid = imregtform(I1, I2, 'rigid', optimizer, metric);
+    tformRigid = imregtform(I1, I2, "rigid", optimizer, metric);
     RA = imref2d(size(I1));
     RB = imref2d(size(I2));
-    I1_rigid = imwarp(I1, RA, tformRigid, 'linear', 'OutputView', RB);
-    M1_rigid = imwarp(M1,   RA, tformRigid, 'nearest', 'OutputView', RB);
+    I1_rigid = imwarp(I1, RA, tformRigid, "linear", "OutputView", RB);
+    M1_rigid = imwarp(M1,   RA, tformRigid, "nearest", "OutputView", RB);
 
     diffImg = abs(I2 - I1_rigid);
-    if exist('imgradient','file')
+    if exist("imgradient","file")
         G = imgradient(I2);
     else
         [gx, gy] = gradient(I2);
@@ -37,27 +37,27 @@ function warp_mask_with_demons(frame1Path, frame2Path, mask1Path, outMaskPath)
     iters = [60 30 10];
     accFieldSmooth = 2.0;
 
-    [D, movingReg] = imregdemons( ...
+    [D, ~] = imregdemons( ...
         I1, I2, iters, ...
-        'AccumulatedFieldSmoothing', accFieldSmooth, ...
-        'PyramidLevels',             numel(iters), ...
-        'DisplayWaitbar',            false);
+        "AccumulatedFieldSmoothing", accFieldSmooth, ...
+        "PyramidLevels",             numel(iters), ...
+        "DisplayWaitbar",            false);
 
-    [M1_warp, ~] = imwarp(M1_rigid, D, 'nearest');
+    [M1_warp, ~] = imwarp(M1_rigid, D, "nearest");
 
     if ~isequal(size(M1_warp), size(I2))
-        M1_warp = imresize(M1_warp, size(I2), 'nearest');
+        M1_warp = imresize(M1_warp, size(I2), "nearest");
     end
 
     M_out = M1_warp;
     M_out(freeze) = M1_rigid(freeze);
 
-    if exist('strel','file') && exist('imclose','file')
-        M_out = imclose(M_out, strel('disk',1));
+    if exist("strel","file") && exist("imclose","file")
+        M_out = imclose(M_out, strel("disk",1));
     end
 
     if nargin < 4 || isempty(outMaskPath)
-        outMaskPath = 'warped_mask.png';
+        outMaskPath = "warped_mask.png";
     end
     imwrite(logical(M_out), outMaskPath);
 
@@ -67,24 +67,39 @@ function warp_mask_with_demons(frame1Path, frame2Path, mask1Path, outMaskPath)
     if size(grid,3) == 1
         G_resized = imresize(grid, size(I2));
         G_resized = im2double(G_resized);
-        G_warped  = imwarp(G_resized, D, 'linear');
+        G_warped  = imwarp(G_resized, D, "linear");
     else
         G_resized = im2double(imresize(grid, [size(I2,1) size(I2,2)]));
-        G_warped  = imwarp(G_resized, D, 'linear');
+        G_warped  = imwarp(G_resized, D, "linear");
     end
     imwrite(G_warped, "deformationVector.png");
 
 
 
-    figure('Name','Rigid+Demons with freeze gating','Color','w');
-    tiledlayout(2,3,'Padding','compact','TileSpacing','compact');
-    nexttile; imshow(I1,[]);                   title('Frame 1');
-    nexttile; imshow(I2,[]);                   title('Frame 2');
-    nexttile; imshow(grid,[]);                 title('Grid');
+    figure("Name","Rigid+Demons with freeze gating","Color","w");
+    tiledlayout(2,3,"Padding","compact","TileSpacing","compact");
+    nexttile; imshow(I1,[]);
+    title("Source Image")
+    nexttile; imshow(I2,[]);
+    title("Target Image")
+    nexttile; imshow(grid,[]);
+    title("Grid (For Reference)")
     
-    nexttile; imshow(M1);                      title('Mask');
-    nexttile; imshow(M1_warp);                 title('Mask after Demons');
-    nexttile; imshow(G_warped,[]);  title('Deformation Vector');
+    % tile 4
+    ax4 = nexttile;
+    imshow(I1,[],"Parent",ax4);
+    hold(ax4,"on");
+    h1 = imshow(M1,"Parent",ax4);
+    set(h1,"AlphaData",0.5);
+    title("Source Mask on Source Image")
 
-    fprintf('Saved warped mask to: %s\n', outMaskPath);
-end
+    % tile 5
+    ax5 = nexttile;
+    imshow(I2,[],"Parent",ax5);
+    hold(ax5,"on");
+    h2 = imshow(M1_warp,"Parent",ax5);
+    set(h2,"AlphaData",0.5);
+    title("Generated Mask on Target Image")
+
+    nexttile; imshow(G_warped,[]);  title("Deformation Vector")
+ end
