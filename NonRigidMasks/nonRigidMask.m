509b2f70ffe207e68f6c74b55ace4f46829ddf62
lancelot.doucet <lancelot.dou7@gmail.com>
2025-09-09 13:56:13 +0200

    feat: benchmarked gradient descent and tweaked values for better results

--- NonRigidMasks/nonRigidMask.m
+++ NonRigidMasks/nonRigidMask.m
@@ -1,13 +1,10 @@
-function nonRigidMask(refImg, targImg, maskIn, maskOut)
-    I1 = imread(refImg);
-    I2 = imread(targImg);
-    M1 = imread(maskIn);
+function nonRigidMask(I1, I2, M1, maskOutPath)
 
     if size(M1,3) == 3
         M1 = rgb2gray(M1);
     end
 
-    % convert image format to rgb
+    % convert image format to gray
     if size(I1,3) == 3
         I1 = rgb2gray(I1);
     end
@@ -18,6 +15,7 @@ function nonRigidMask(refImg, targImg, maskIn, maskOut)
     I1 = im2double(I1);
     I2 = im2double(I2);
 
+    tic;
     [optimizer, metric] = imregconfig("monomodal");
     optimizer.MaximumIterations = 100; % this value can be tweaked for either faster or better results
     tformRigid = imregtform(I1, I2, "rigid", optimizer, metric);
@@ -25,7 +23,10 @@ function nonRigidMask(refImg, targImg, maskIn, maskOut)
     RB = imref2d(size(I2));
     I1_rigid = imwarp(I1, RA, tformRigid, "linear", "OutputView", RB);
     M1_rigid = imwarp(M1,   RA, tformRigid, "nearest", "OutputView", RB);
+    t1 = toc;
+    disp(t1)
 
+    tic;
     diffImg = abs(I2 - I1_rigid);
     if exist("imgradient","file")
         G = imgradient(I2);
@@ -33,12 +34,12 @@ function nonRigidMask(refImg, targImg, maskIn, maskOut)
         [gx, gy] = gradient(I2);
         G = hypot(gx, gy);
     end
-    tauDiff = 3/255;
+    tauDiff = 10/255;
     tauGrad = 0.02;
     freeze = (diffImg <= tauDiff) & (G <= tauGrad);
 
-    iters = [5 3];
-    accFieldSmooth = 10.0;
+    iters = [5 15 30];
+    accFieldSmooth = 5.0;
 
     [D, ~] = imregdemons( ...
         I1, I2, iters, ...
@@ -54,22 +55,23 @@ function nonRigidMask(refImg, targImg, maskIn, maskOut)
 
     M_out = M1_warp;
     M_out(freeze) = M1_rigid(freeze);
+    t2 = toc;
+    disp(t2)
 
     if exist("strel","file") && exist("imclose","file")
         M_out = imclose(M_out, strel("disk",1));
     end
 
-    if nargin < 4 || isempty(maskOut)
-        maskOut = "warped_mask.png";
+    if nargin < 4 || isempty(maskOutPath)
+        maskOutPath = "warped_mask.png";
     end
-    imwrite(logical(M_out), maskOut);
+    imwrite(logical(M_out), maskOutPath);
 
     gridSpacing = 50;
     grid = checkerboard(gridSpacing, ceil(size(I2,1)/(2*gridSpacing)), ceil(size(I2,2)/(2*gridSpacing)));
     grid = imresize(grid, size(I2));
     grid = im2double(grid);
-    G_warped = imwarp(grid, D*5, "linear");
-    imwrite(G_warped, "deformationVector.png");
+    G_warped = imwarp(grid, D, "linear");
 
     figure("Name","Rigid+Demons with freeze gating","Color","w");
     tiledlayout(2,3,"Padding","compact","TileSpacing","compact");
@@ -93,4 +95,15 @@ function nonRigidMask(refImg, targImg, maskIn, maskOut)
     title("Mask after Demons on Target Image")
 
     nexttile; imshow(G_warped,[]);  title("Deformation Vector")
- end
+end
+
+%{
+benchmark results:
+for 20 MaximumIterations: gradiant descent 2.9335s, non-rigid updates 0.2519s
+
+for 50 MaximumIterations: gradiant descent 6.2712s, non-rigid updates 0.2471s
+
+for 100 MaximumIterations: gradiant descent 12.1143s, non-rigid updates 0.2593s
+
+for 200 MaximumIterations: gradiant descent 24.3504s, non-rigid updates 0.2485s
+%} 
