3fd9842ef35e01d3034da7add8b61c0e792d7082
Yann Fischer <yann.fischer.pro@gmail.com>
2025-09-23 15:23:40 +0200

    Refactor file reading logic to handle multiple data formats and improve error handling; update version to v2.9.0

--- Loading/readHDF5.m
+++ Loading/readHDF5.m
@@ -1,30 +1,56 @@
 function obj = readHDF5(obj)
 
 dir_path_raw = fullfile(obj.directory, 'raw');
-NameRefRawFile = strcat(obj.filenames, '_raw.h5');
-RefRawFilePath = fullfile(dir_path_raw, NameRefRawFile);
+
+% Search for all .h5 files in the folder
+h5_files = dir(fullfile(dir_path_raw, '*.h5'));
+
+if isempty(h5_files)
+    error('No HDF5 file was found in the folder: %s', dir_path_raw);
+end
+
+% Takes the first .h5 file found
+RefRawFilePath = fullfile(dir_path_raw, h5_files(1).name);
+
+fprintf('Reading the HDF5 file: %s\n', h5_files(1).name);
 
 try
-    %h5disp(RefRawFilePath)
+    info = h5info(RefRawFilePath, '/');
+    dataset_names = {info.Datasets.Name};
 
-    obj.M0_raw_video = squeeze(h5read(RefRawFilePath, '/moment0'));
-    obj.M1_raw_video = squeeze(h5read(RefRawFilePath, '/moment1'));
-    obj.M2_raw_video = squeeze(h5read(RefRawFilePath, '/moment2'));
-    %obj.M0_ff_raw_video = obj.M0_raw_video;
+    if ismember('moment0', dataset_names)
+        fprintf('Reading the M0 data\n');
+        obj.M0_raw_video = squeeze(h5read(RefRawFilePath, '/moment0'));
+    else
+        fprintf('Warning: moment0 dataset not found\n');
+    end
+
+    if ismember('moment1', dataset_names)
+        fprintf('Reading the M1 data\n');
+        obj.M1_raw_video = squeeze(h5read(RefRawFilePath, '/moment1'));
+    else
+        fprintf('Warning: moment1 dataset not found\n');
+    end
+
+    if ismember('moment2', dataset_names)
+        fprintf('Reading the M2 data\n');
+        obj.M2_raw_video = squeeze(h5read(RefRawFilePath, '/moment2'));
+    else
+        fprintf('Warning: moment2 dataset not found\n');
+    end
+
+    if ismember('SH', dataset_names)
+        fprintf('Reading the SH data\n');
+        obj.SH_data_hypervideo = squeeze(h5read(RefRawFilePath, '/SH'));
+    else
+        fprintf('Warning: SH dataset not found\n');
+    end
 
 catch ME
 
     disp(['ID: ' ME.identifier])
     rethrow(ME)
 
-end
-
-try
-    % Import SH data
-    obj.SH_data_hypervideo = h5read(RefRawFilePath, '/SH');
-
-catch
-    fprintf(2, 'Warning: no SH was found\n');
 end
 
 end
