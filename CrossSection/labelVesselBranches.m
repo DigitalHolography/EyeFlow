626c072fc970f74f7565e44240d2c27e3aa30d19
Yann Fischer <yann.fischer.pro@gmail.com>
2025-10-01 16:57:41 +0200

    Enhance labelVesselBranches function to accept additional parameters and update createMasks to utilize new functionality for vessel signal extraction

--- CrossSection/labelVesselBranches.m
+++ CrossSection/labelVesselBranches.m
@@ -1,9 +1,31 @@
-function [labeledVessels, n] = labelVesselBranches(vesselMask, maskSection, xy_barycenter)
+function [labeledVessels, n] = labelVesselBranches(vesselMask, maskSection, xy_barycenter, fun_params)
+% labelVesselBranches - Label vessel branches in a binary vessel mask using watershed segmentation.
+%
+% Syntax:
+%   [labeledVessels, n] = labelVesselBranches(vesselMask, maskSection, xy_barycenter)
+%
+% Inputs:
+%   vesselMask     - A binary mask (logical array) of the segmented vessels.
+%   maskSection    - A binary mask (logical array) defining the cross-sectional area of interest.
+%   xy_barycenter  - A 2-element vector [x, y] representing the center of the cross-section.
+%
+% Outputs:
+%   labeledVessels - A labeled mask (integer array) where each vessel branch has a unique label.
+%   n              - The number of labeled vessel branches. 0 if none are found.
+
+arguments
+    vesselMask logical % Binary mask of the segmented vessels
+    maskSection logical % Binary mask defining the cross-sectional area of interest
+    xy_barycenter (1, 2) double % [x, y] coordinates of the center of the cross-section
+    fun_params.strelSize double = 2 % Size for structuring element in morphological operations
+    fun_params.smallBranchCriteria double = 10 % Minimum area (in pixels) for a branch to be considered valid
+end
 
 % Get parameters from global toolbox
 ToolBox = getGlobalToolBox;
 params = ToolBox.getParams;
 
+% Get image size and define parameters
 [numX, numY] = size(vesselMask);
 x_c = xy_barycenter(1);
 y_c = xy_barycenter(2);
@@ -19,11 +41,13 @@ skel = skel & ~cercleMask;
 
 % Remove branch points from skeleton
 branchPoints = bwmorph(skel, 'branchpoints');
-skelNoBranchesPoints = skel & ~imdilate(branchPoints, strel('disk', 2));
+skelNoBranchesPoints = skel & ~imdilate(branchPoints, strel('disk', fun_params.strelSize));
 
 % Remove small branches
-skelNoBranchesPoints = bwareaopen(skelNoBranchesPoints, 10); % Remove small branches
-[label, n] = bwlabel(skelNoBranchesPoints); % Label disconnected branchess
+skelNoBranchesPoints = bwareaopen(skelNoBranchesPoints, fun_params.smallBranchCriteria); % Remove small branches
+
+% Label remaining branches
+[label, n] = bwlabel(skelNoBranchesPoints);
 
 % Compute distance transform (vessel thickness)
 D = -bwdist(~vesselMask); % Negative transform for watershed
