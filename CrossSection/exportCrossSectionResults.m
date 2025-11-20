074a7260c3e2285ae3510cb8460e1f7d7b5579b8
lance-leau <lancelot.dou7@gmail.com>
2025-11-20 15:34:27 +0100

    chore: added a generalized exportSegmentsValueToH5 function

--- CrossSection/exportCrossSectionResults.m
+++ CrossSection/exportCrossSectionResults.m
@@ -116,6 +116,17 @@ if params.json.exportCrossSectionResults.BloodFlowProfilesOverlay && saveFigures
     profilePatchVelocities(v_profiles_cell, name, locsLabel, mean(M0_ff, 3))
 end
 
+alphaWom = zeros(size(ToolBox.Cache.WomersleyOut),'single');
+for i = 1:size(alphaWom, 1)
+    for j = 1:size(alphaWom, 2)
+        if isstruct(ToolBox.Cache.WomersleyOut{i,j})
+            data = ToolBox.Cache.WomersleyOut(i, j);
+            alphaWom(i, j) = data{1, 1}.alpha_n;
+        end
+    end
+end
+exportSegmentsValueToH5("Wom_alpha",maskLabel,alphaWom,"Womersley");
+
 fprintf("    1.(bis) optional Flow Rate Figures (interpolated velocity profiles / Histograms / Profiles Overlay) (%s) took %ds\n", name, round(toc))
 
 % 2. Sections Figures
---
+++ Tools/exportSegmentsValueToH5.m
@@ -0,0 +1,26 @@
+function exportSegmentsValueToH5(name,maskLabel,values,baseName)
+
+if nargin < 4
+    baseName = "Segments";
+end
+
+if ~endsWith(baseName, "/") && ~endsWith(baseName, "_")
+    baseName = baseName + "/";
+end
+
+ToolBox = getGlobalToolBox;
+
+[numCircles, numBranches] = size(maskLabel);
+typeName = class(values(1));
+
+L = zeros(size(maskLabel{1,1}), typeName);
+% Process each circle and branch
+for cIdx = 1:numCircles
+    for bIdx = 1:numBranches
+        L(maskLabel{cIdx, bIdx}) = values(cIdx, bIdx);
+    end
+end
+
+ToolBox.Output.Extra.add(baseName + sprintf("%s_Segments_Labels", name), L);
+
+end
