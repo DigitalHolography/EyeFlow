198797cbdfb32810843cb2f23b9cae5c07d88bfa
Marius Dubosc <mariusdubosc@gmail.com>
2025-11-25 16:40:58 +0100

    fix(model laoding): ensure python verison is correct

--- Preprocessing/loadAVSegmentationNetworks.m
+++ Preprocessing/loadAVSegmentationNetworks.m
@@ -8,15 +8,31 @@ function [model_struct] = loadAVSegmentationNetworks(params)
 
 use_python = false;
 extension = ".onnx";
+
 try
-    % Try using Python
-    pyver = pyenv;  % If this fails, Python isn't configured
+    % Try detecting Python
+    pyver = pyenv;
+    
     fprintf("Python detected: %s\n", pyver.Version);
-    use_python = true;
-    extension = ".pt";
-catch
-    warning("Python not detected or not configured properly. Out of date .onnx models will be used.");
+
+    % Parse version string "3.x.y"
+    v = sscanf(pyver.Version, "%d.%d.%d");
+    major = v(1);
+    minor = v(2);
+
+    % Check version range: 3.10 ≤ version < 3.13
+    if major == 3 && minor >= 10 && minor < 13
+        use_python = true;
+        extension = ".pt";
+        fprintf("Using PyTorch model (.pt)\n");
+    else
+        warning("Python version %s is not supported ; it must be >= 3.10 and < 3.13 (3.12 recommanded). Using ONNX.", pyver.Version);
+    end
+
+catch ME
+    warning("Python not detected or not configured. Using ONNX instead.\n%s", ME.message);
 end
+
 
 
 if params.json.Mask.AVCorrelationSegmentationNet && params.json.Mask.AVDiasysSegmentationNet
